73d13b8973e17ebf14b04301419aafdb8835abbe
Dejon Wright <dejon.wright@gmail.com>
2024-08-11 16:39:13 -0400

    allow custom session to be passed into base client constructor

--- packages/core/src/clients/base.ts
+++ packages/core/src/clients/base.ts
@@ -1,20 +1,16 @@
 import axios, {AxiosInstance} from "axios";
 
 export class SportsSdkClient {
-    protected readonly endpoint: string;
+    public readonly endpoint: string;
     public session: AxiosInstance;
 
-    constructor(endpoint: string) {
+    constructor(endpoint: string, session?: AxiosInstance) {
         this.endpoint = endpoint;
-        this.session = axios.create({
+        this.session = session || axios.create({
             baseURL: this.endpoint,
             headers: {
                 "Content-Type": "application/json",
             },
-			validateStatus: function(status) {
-                // Axios marks 304 as an error, but clients should accept and handle 304
-				return status < 300 || status == 304;
-			},
         })
     }
 }
