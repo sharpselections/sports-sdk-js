e252c1497fcb368f426bf81161b5c523c9173cf2
Dejon Wright <dejon.wright@gmail.com>
2024-12-09 19:23:16 -0500

    exclude testing deps from built sleeper package

--- packages/sleeper/tsup.config.ts
+++ packages/sleeper/tsup.config.ts
@@ -7,7 +7,15 @@ export default defineConfig((options: Options) => ({
 	splitting: false, // Disable code splitting for simplicity
 	sourcemap: false, // Optionally enable sourcemaps
 	clean: true, // Clean output directory before building
-	external: [], // List your external dependencies here
+	external: [
+		// Mark test-related packages as external
+		"nock",
+		"jest",
+		"ts-jest",
+		// Also exclude other Node.js-only dependencies
+		"util",
+		"process"
+	],
 	treeshake: true, // Enable tree-shaking
 	target: "esnext", // Target modern JavaScript
 	outDir: "dist", // Ensure output directory is correctly set
